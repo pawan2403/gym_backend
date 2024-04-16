99ced51df568a64b04fdacbbefb2f3c7a97885d7
pawan <pk01641@gmail.cm>
2024-04-17 01:43:32 +0530

    add oooo port

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const http = require('http');
 const app = require('./index');
 const server = http.createServer(app);
-const functions = require("firebase-functions")
+// const functions = require("firebase-functions")
 
 server.listen(process.env.PORT, ()=>{
     console.log(`connection is setup at ${process.env.PORT}`)
 })
 
-exports.api = functions.https.onRequest(app)
+// exports.api = functions.https.onRequest(app)
